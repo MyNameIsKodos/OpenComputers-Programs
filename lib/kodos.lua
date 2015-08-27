8853e98147c2542f003977141e7cb42ac84ce1c6
MyNameIsKodos <jge84@yahoo.com>
2015-08-27 07:43:01 -0700

    Cleanup and formatting. THE OCD GODS DEMAND PERFECTION

--- lib/kodos.lua
+++ lib/kodos.lua
@@ -3,23 +3,24 @@ local serialization = require("serialization")
 local gpu = component.gpu
 local screen = component.screen
 
-
--- Ignore these four words
-
 local kodos = {}
-kodos.math = {}
-kodos.textutils = {}
-kodos.miscutils = {}
-kodos.fileutils = {}
-kodos.networkutils = {}
+  kodos.math = {}
+  kodos.textutils = {}
+  kodos.miscutils = {}
+  kodos.fileutils = {}
+  kodos.networkutils = {}
 
-function kodos.textutils.str2hex(a)                 -- textutils
+
+
+-- Text Utility Functions --
+
+function kodos.textutils.str2hex(a)
   return a:gsub(".",function(a)
     return string.format("%02x",a:byte())
   end)
 end
 
-function kodos.textutils.hex2str(a)                 -- textutils
+function kodos.textutils.hex2str(a)
   return a:gsub("..",function(a)
     return string.char(tonumber(a,16))
   end)
@@ -39,19 +40,26 @@ function kodos.textutils.justify(a,b,c)
   end
 end
 
-function kodos.math.round(num, idp)            -- math
+
+-- Non-standard Math Functions --
+
+function kodos.math.round(num, idp)           
   local mult = 10^(idp or 0)
   return math.floor(num * mult + 0.5) / mult
 end
 
-function kodos.miscutils.dump(mytab)                -- Not even sure what to categorize this into. Using it for method dump atm
+-- Miscellaneous Utility Functions --
+
+function kodos.miscutils.dump(mytab)          
   for k,v in pairs(mytab) do
     print(k,v)
   end
   return
 end
 
-function kodos.fileutils.readFile(filename)         -- fileutils
+-- File Utitily Functions --
+
+function kodos.fileutils.readFile(filename)   
   local file, err = io.open(filename,"rb")
   if not file then
     return nil, err
@@ -70,28 +78,16 @@ function kodos.fileutils.writeToFile(data,filename,overwrite)
   fhand:close()
 end
 
---if component.isAvailable("data") or component.isAvailable("os_datablock") then
+-- Networking Functions --
 
---function kodos.networkutils.prepare(data)        -- TODO: Actually write the functions
---serdata = serialization.serialize(data)
---defdata = component.data.deflate(serdata) or component.os_datablock.deflate(serdata)
---return defdata
---end
---function kodos.networkutils.receive(data)
---infdata = component.data.inflate(data) or component.os_datablock.deflate(data)
---unserdata = serialization.unserialize(infdata)
---return unserdata
---end
---else
-
-function kodos.networkutils.prepare(data)
-serdata = serialization.serialize(data)
-return serdata
+function kodos.networkutils.prepare(data)   -- I realize this is just a wrapped serialize and unserialize function.
+  serdata = serialization.serialize(data)     -- This lib is mostly for personal use, however anyone is free to use it, 
+  return serdata                              -- or any code contained within.  - Kodos
 end
 
 function kodos.networkutils.receive(data)
-unserdata = serialization.unserialize(data)
-return unserdata
+  unserdata = serialization.unserialize(data)
+  return unserdata
 end
 
 return kodos
