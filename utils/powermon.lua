f094a4dfec0a5be195d5fe02fa097eaad756be16
Kodos Macarthur <MyNameIsKodos@users.noreply.github.com>
2016-07-20 15:48:44 -0500

    Update powermon.lua

--- utils/powermon.lua
+++ utils/powermon.lua
@@ -1,4 +1,4 @@
--- Version 0.51
+-- Version 0.52
 
 -- Changelog:
 -- Added TODO List
@@ -6,16 +6,16 @@
 -- Updated Pastebin with current code
 -- Actually fixed indentation
 -- Made lamp colours gradients
-
+-- Added Light Board support
+-- Made colorful lamps optional
 
 -- ==================================
 
 -- TODO
 -- Add a way to have a second lamp track net gain/loss of power
--- Make Lamp(s) optional *still*
--- Refactor (Is this even the right word) term.write to use 
+-- Refactor (Is this even the right word) term.write to use
 -- gpu.set instead, as well as setting it up so that only the
--- values that change every update will be redrawn, instead of 
+-- values that change every update will be redrawn, instead of
 -- the entire GUI
 -- Make things prettier on the front end
 
@@ -27,10 +27,8 @@ local term = require("term")
 
 local gpu = component.gpu            -- Note that this program requires a T3 Screen.
 
-local lamp = component.colorful_lamp -- As of right now, a Computronics Lamp is required. 
-                                     -- I may take another stab at making it optional later on,
-                                     -- but for now, it's required or the program will not run.
-									 
+local lamp =  component.isAvailable("colorful_lamp") and component.colorful_lamp or nil
+
 local blue = 0x1F
 
 local bit32 = bit32 or load([[return {
@@ -64,12 +62,14 @@ end
 
 local function updateMon(curr, maxStorage)
   term.clear()
-  term.setCursor(1,1)
-  io.stdout:write(curr .. "/" .. maxStorage .. " RF Stored.")
+  local text = curr .. "/" .. maxStorage .. " RF Stored"
+  term.setCursor(math.ceil((40 - #text) / 2), 1)
+  io.stdout:write(text..".")
   return
 end
 
 local function updateLamp(curr, maxStorage)
+  if not lamp then return end
   if curr == 0 and maxStorage == 0 then
     lamp.setLampColor(blue)
     return
@@ -81,7 +81,30 @@ local function updateLamp(curr, maxStorage)
   end
 end
 
-function updateReactor()  -- Code WIP
+local light = component.isAvailable("light_board") and component.light_board or nil
+local prevCount = 0
+
+local function updateLight()
+  if not light then return prevCount end
+  local count = light.light_count
+  if count == prevCount then return count end
+  prevCount = count
+  for i = 1, count do
+    local perc = i / count
+    local red = math.max(math.min(math.ceil(0xE0 * ((1 - perc)*2)), 0xE0), 0)
+    local green = math.max(math.min(math.ceil(0xFF * (perc * 2)), 0xFF), 0)
+    light.setColor(i, bit32.bor(bit32.lshift(red, 16), bit32.lshift(green, 8)))
+  end
+  return count
+end
+
+local function updateDisplay(curr, maxStorage)
+  if not light then return end
+  local count = updateLight()
+  local perc = curr / maxStorage
+  for i = 1, count do
+    light.setActive(i, i <= math.ceil(perc * count))
+  end
 end
 
 while true do
@@ -89,6 +112,7 @@ while true do
   local curr = checkBatt()
   updateMon(curr, maxStorage)
   updateLamp(curr, maxStorage)
+  updateDisplay(curr, maxStorage)
   os.sleep(1)
 end
 
